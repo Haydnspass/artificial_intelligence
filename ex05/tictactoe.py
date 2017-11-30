1c82e1669c688a2933037fedafad08a32832be6e
Lucas-Rapahel Müller <gitdev@LRM.photo>
2017-11-30 19:07:58 +0100

    2nd draft

--- ex05/tictactoe.py
+++ ex05/tictactoe.py
@@ -12,18 +12,16 @@ class Board(object):
 
     win_count = 3
 
-    def __init__(self):
-        self.board = np.empty((board_rows, board_cols), dtype=str)
+    def __init__(self, board=np.empty((board_rows, board_cols), dtype=str)):
+        self.board = board
 
     def possible_moves(self):
-        if self.next_player == self.PLAYER_2:
-            if self.check_winner(self.PLAYER_1):
-                return []
-        else:
-            if self.check_winner(self.PLAYER_2):
-                return []
+        if self.check_winner(self.PLAYER_1) or self.check_winner(self.PLAYER_2):
+            return []
 
         is_empty = np.stack(np.where(self.board == ''), axis=1)
+        if is_empty.size == 0:
+            return []
         return is_empty
 
     def parse(self, move, player, deep=False):
@@ -37,6 +35,10 @@ class Board(object):
     @property
     def num_coins(self):
         return np.sum(b.board == self.PLAYER_1) + np.sum(b.board == self.PLAYER_2)
+
+    @property
+    def current_player(self):
+        return self.PLAYER_2 if self.num_coins % 2 == 0 else self.PLAYER_1
 
     @property
     def next_player(self):
@@ -66,15 +68,15 @@ class Board(object):
             return False
 
         for i in range(board_rows):
-            if np.sum(self.board[i,:] == player) >= win_count:
+            if np.sum(self.board[i,:] == player) >= self.win_count:
                 return True
         for i in range(board_cols):
-            if np.sum(self.board[:,i] == player) >= win_count:
+            if np.sum(self.board[:,i] == player) >= self.win_count:
                 return True
         # maybe add nebendiagonalen later
-        if np.sum(np.diag(self.board) == player) >= win_count:
+        if np.sum(np.diag(self.board) == player) >= self.win_count:
             return True
-        if np.sum(np.diag(np.fliplr(self.board)) == player) >= win_count:
+        if np.sum(np.diag(np.fliplr(self.board)) == player) >= self.win_count:
             return True
 
         return False
@@ -83,27 +85,30 @@ class Board(object):
 
 
 
-def minimax(node, depth, maxi_player, board):
-    if depth == 0 or childs == []:
-        return heuristic(node)
+def minimax(board, depth, maxi_player):
+    if depth == 0 or board.possible_moves() == []:
+        return (board.utility(board.current_player, mode='hard'), board.board)
 
     if maxi_player:
         best_v = -np.inf
-        for c in childs(node):
-            clone = deepcopy(board)
+        for c in board.possible_moves():
+            clone = Board(board.board)
             clone.parse(c, clone.next_player)
-            v = minimax(c, depth - 1, False)
+            v, _ = minimax(clone, depth - 1, False)
             best_v = max(best_v, v)
-        return best_v
+        return (best_v, board.board)
 
     else:
         best_v = np.inf
-        for c in childs(node):
-            v = minimax(c, depth - 1, True)
+        for c in board.possible_moves():
+            clone = Board(board.board)
+            clone.parse(c, clone.next_player)
+            v, _ = minimax(clone, depth - 1, True)
             best_v = min(best_v, v)
-        return best_v
+        return (best_v, board.board)
 
 
 if __name__ == '__main__':
     b = Board()
-
+    v, b = minimax(b, 7, True)
+    print(v, b)
